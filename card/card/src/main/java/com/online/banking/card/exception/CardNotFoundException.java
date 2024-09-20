4ca99f00ca96c0ed71dbecb8344f1425b30dff44
XI3287-PreethiS <96439275+XI3287-PreethiS@users.noreply.github.com>
2024-09-20 11:31:34 +0530

    Delete card/card directory

--- card/card/src/main/java/com/online/banking/card/exception/CardNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.online.banking.card.exception;
-
-
-public class CardNotFoundException extends RuntimeException {
-    public CardNotFoundException(String message) {
-        super(message);
-    }
-}
-
--- card/card/src/main/java/com/online/banking/card/exception/InvalidCardException.java
+++
@@ -1,8 +0,0 @@
-package com.online.banking.card.exception;
-
-public class InvalidCardException extends RuntimeException {
-    public InvalidCardException(String message) {
-        super(message);
-    }
-}
-
