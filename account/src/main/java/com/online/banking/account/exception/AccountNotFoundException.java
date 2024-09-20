50cacdd9b0dd7c2d13375a6d63cee89533332de5
XI3287-PreethiS <96439275+XI3287-PreethiS@users.noreply.github.com>
2024-09-20 11:27:50 +0530

    Delete account/src directory

--- account/src/main/java/com/online/banking/account/exception/AccountNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.online.banking.account.exception;
-
-public class AccountNotFoundException extends RuntimeException {
-    public AccountNotFoundException(String message) {
-        super(message);
-    }
-}
